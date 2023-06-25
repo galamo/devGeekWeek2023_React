642e1ae1607866259412316a0f3f74b3ae532850
galamouyal <galamouyal88@gmail.com>
2023-06-25 14:40:46 +0300

    card

--- react-client/src/components/pages/countries/countriesList.tsx
+++ react-client/src/components/pages/countries/countriesList.tsx
@@ -1,9 +1,11 @@
+import { CountryCard } from "../../ui-components/countryCard"
+import css from "./countriesList.module.css"
 export function CountriesList(props: { countries: Array<any> }) {
-    return <>
-        {props.countries.map((country: { name: { common: string } }) => {
-            return <div>
-                {country?.name?.common}
-            </div>
+    return <div className={css.listWrapper}>
+        {props.countries.map((country: { flags: { png: string }, region: string, name: { common: string } }) => {
+            return <CountryCard name={country?.name?.common}
+                flag={country?.flags?.png}
+                region={country.region} />
         })}
-    </>
+    </div>
 }
