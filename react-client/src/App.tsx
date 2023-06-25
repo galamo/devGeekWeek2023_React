9c1ccc12c9f2e8e8ea55ec408134cb468ad1c054
galamouyal <galamouyal88@gmail.com>
2023-06-25 13:31:58 +0300

    a

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,14 +1,37 @@
 import './App.css'
+import { useState } from "react"
 import { Header } from './components/ui-components/header'
 
 function App() {
-
+    console.log("App component rendered")
+    const defaultValue: string = "Default"
+    const [currentValue, setCurrentValue] = useState(defaultValue)
+    // const text = "Countries List"
+    const inputHandler = (event: any) => {
+        setCurrentValue(event?.target?.value)
+    }
     return (
         <div>
-            <Header text='Countries List' color={"yellow"} />
+            <input type='text' onChange={inputHandler} />
+            <Header text={currentValue} color={"green"} />
             <Header text='Products List' />
         </div>
     )
 }
 
+
+
 export default App
+
+
+// class Test {
+//     constructor(_name) {
+//         this.name = _name
+//     }
+//     setName(newName) {
+//         this.name = newName
+//     }
+//     getName() {
+//         return this.name
+//     }
+// }
--- react-client/src/components/ui-components/header/index.tsx
+++ react-client/src/components/ui-components/header/index.tsx
@@ -1,11 +1,13 @@
 
+// import "./header.css" DONT USE IT
+import css from "./header.module.css"
 interface IProps {
     text: string;
     color?: string;
 }
 export function Header(props: IProps) {
     const { text, color = "red" } = props;
-    return <h1 style={{ color: color }} >
+    return <h1 className={css.borderUgly} style={{ color: color }} >
         {text}
     </h1>
 }
