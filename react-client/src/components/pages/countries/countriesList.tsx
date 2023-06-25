1beb43b33bf7df0a72d12173dad181f609ba286b
galamouyal <galamouyal88@gmail.com>
2023-06-25 15:44:23 +0300

    useTransition

--- react-client/src/components/pages/countries/countriesList.tsx
+++ react-client/src/components/pages/countries/countriesList.tsx
@@ -3,7 +3,7 @@ import css from "./countriesList.module.css"
 export function CountriesList(props: { countries: Array<any> }) {
     return <div className={css.listWrapper}>
         {props.countries.map((country: { flags: { png: string }, region: string, name: { common: string } }) => {
-            return <CountryCard name={country?.name?.common}
+            return <CountryCard key={country?.name?.common + country?.region} name={country?.name?.common}
                 flag={country?.flags?.png}
                 region={country.region} />
         })}
