6841c7beec037fb88aad0ff8b6c02f01811e2558
galamouyal <galamouyal88@gmail.com>
2023-06-25 16:14:39 +0300

    reouting

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -2,21 +2,51 @@ import './App.css'
 import { useState } from "react"
 import { Header } from './components/ui-components/header'
 import CountriesPage from './components/pages/countries'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { Button } from 'primereact/button'
+import LoginForm from './components/pages/login'
+import NotFound from './components/pages/not-found'
+
+interface IRoute {
+    path: string,
+    key: string,
+    component: any,
+    label?: string
+}
+const routes: Array<IRoute> = [
+    {
+        path: "/login",
+        component: <LoginForm />,
+        key: "login",
+        label: "Login"
+    },
+    {
+        path: "/countries",
+        component: <CountriesPage />,
+        key: "countries",
+        label: "Countries"
+    },
+    {
+        path: "*",
+        component: <NotFound />,
+        key: "not found",
+    }
+]
+
 
 function App() {
-    console.log("App component rendered")
-    const defaultValue: string = "Default"
-    const [currentValue, setCurrentValue] = useState(defaultValue)
-    // const text = "Countries List"
-    const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCurrentValue(event?.target?.value)
-    }
     return (
         <div>
-            <CountriesPage />
-            <input type='text' onChange={inputHandler} />
-            <Header text={currentValue} color={"green"} />
-            <Header text='Products List' />
+            <BrowserRouter>
+                {routes.filter(r => r.label).map((route: IRoute) => {
+                    return <Link key={route.label} to={route.path} > {route.label} </Link>
+                })}
+                <Routes>
+                    {routes.map((route: IRoute) => {
+                        return <Route path={route.path} key={route.key} element={route.component} />
+                    })}
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
